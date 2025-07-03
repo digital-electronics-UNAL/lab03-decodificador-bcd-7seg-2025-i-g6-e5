33e7dedc192bcf733d95d05819e28e56326c92c8
ElKakoGazapo <alequinteroro@unal.edu.co>
2025-07-02 22:16:47 -0500

    topsumrest (4)

--- src/top_sum_res_tb.v
+++ src/top_sum_res_tb.v
@@ -10,6 +10,49 @@ module top_sum_res_tb();
     wire [0:6] SSeg;
     wire [3:0] an;
 
-    
+    top_sum_res TSR (
+        .A(A),
+        .B(B),
+        .Sel(Sel),
+        .clk(clk),
+        .rst(rst),
+        .SSeg(SSeg),
+        .an(an)
+    );
+
+    always #5 clk = ~clk;
+
+    initial begin
+
+        A = 0;
+        B = 0;
+        Sel = 0;
+        clk = 0;
+        rst = 1;
+        #10;
+
+        rst = 0;
+
+        A = 4'd3;
+        B = 4'd2;
+        Sel = 0;
+        #20;
+
+        Sel = 1;
+        #20;
+
+        A = 4'd2;
+        B = 4'd3;
+        Sel = 1;
+        #20;
+
+        A = 4'd8;
+        B = 4'd8;
+        Sel = 0;
+        #20;
+
+        $display("Testbench finalizado.");
+        $finish;
+    end
 
 endmodule
