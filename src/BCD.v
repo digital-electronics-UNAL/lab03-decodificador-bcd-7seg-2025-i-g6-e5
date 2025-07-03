aa9c3fa06b4e4cc34e8813952e8fc0544233021c
Snegunal <snegrete@unal.edu.co>
2025-07-03 13:46:21 -0500

    Final ahora si}

--- src/BCD.v
+++ src/BCD.v
@@ -15,22 +15,20 @@ module BCD (
 		c = 'b0;
   end
   
+ wire [4:0] new_in = (Sel ==1)? ((inp[4]==0)? {1'b0, ~inp[3:0]+4'b0001} : {1'b0, inp[3:0]}) : inp;
+  
 
   always@(posedge clk2) begin
       case(algo)
         0:begin
-          bcd<=inp%10;
-          algo <= 1;
-			 c <= 4'b1110;
+			 	 bcd<=(new_in)%10;
+				 algo <= 1;
+				 c <= 4'b1110;
           end
         1:begin
-          bcd<=(inp-inp%10)/10;
-          if (Sel ==1 && inp[4]==0) begin
-            algo <= 2'b10;
-          end else begin
-            algo <= 0;
-          end
-			 c <= 4'b1101;
+            bcd<=(new_in-new_in%10)/10;
+				algo <= (Sel ==1 & inp[4]==0)? 2 :0;
+			   c <= 4'b1101;
           end
         2: begin
           bcd <= 4'b1100;
