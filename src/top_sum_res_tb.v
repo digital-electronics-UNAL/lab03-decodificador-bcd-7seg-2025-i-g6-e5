f3d213588e7e3639b15d167f7fe54a2f5900f00b
Alessandra Quintero Rois <alequinteroro@unal.edu.co>
2025-07-02 22:12:56 -0500

    topsumrest (3)

---
+++ src/top_sum_res_tb.v
@@ -0,0 +1,15 @@
+`timescale 1ns/1ps
+
+module top_sum_res_tb();
+    reg [3:0] A;
+    reg [3:0] B;
+    reg Sel;
+    reg clk;
+    reg rst;
+    
+    wire [0:6] SSeg;
+    wire [3:0] an;
+
+    
+
+endmodule
