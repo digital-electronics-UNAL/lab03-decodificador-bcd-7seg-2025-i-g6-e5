36109fb968b6e95b40ed0874480adf89342425c1
Alessandra Quintero Rois <alequinteroro@unal.edu.co>
2025-07-03 00:02:46 -0500

    topsumrest (14)

--- src/tb_top.v
+++ src/tb_top.v
@@ -1,26 +1,23 @@
 `timescale 1ns/1ps //(unidad de tiempo)/(resolución)
-`include "src/BCD.v"
-`include "src/BCDtoSSeg.v"
-`include "src/CF_Div.v"
 `include "src/top.v"
 
 module tb_top ();
-    reg [3:0] inp;
+    reg [4:0] inp;
     reg clk2;
     reg rst;
     wire [0:6] SSeg;
     wire [3:0] an;
 
     top uut(
-    .inp(inp),
-    .clk(clk2),
-    .rst(rst),
-    .SSeg(SSeg),
-    .an(an)
+      .inp(inp),
+      .clk(clk2),
+      .rst(rst),
+      .SSeg(SSeg),
+      .an(an)
     );
 
     initial begin 
-        inp = 4'b1111;
+        inp = 5'b11111;
         clk2 = 0;
         rst = 0;
         #10 rst = 0;  // libera el reset  
