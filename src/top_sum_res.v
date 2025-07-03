ba341b89fa5379d45f4f3c247e8a5fd073100828
Alessandra Quintero Rois <alequinteroro@unal.edu.co>
2025-07-02 22:10:34 -0500

    topsumrest (2)

---
+++ src/top_sum_res.v
@@ -0,0 +1,36 @@
+`include "src/sum_res_4b.v"
+`include "src/top.v"
+
+module top_sum_res(
+    input [3:0] A,
+    input [3:0] B,
+    input Sel,
+    input clk,
+    input rst,
+    output [0:6] SSeg,
+    output [3:0] an
+    );
+
+    wire Cout;
+    wire [3:0] S;
+    wire [4:0] inp_top;
+
+    assign inp_top = {Cout, S};
+
+    sum_res_4b sr (
+        .A(A),
+        .B(B),
+        .Sel(Sel),
+        .Cout(Cout),
+        .S(S)
+    );
+
+    top u_top (
+        .inp(inp_top)
+        .clk(clk)
+        .rst(rst)
+        .SSeg(SSeg)
+        .an(an)
+    );
+    
+endmodule
