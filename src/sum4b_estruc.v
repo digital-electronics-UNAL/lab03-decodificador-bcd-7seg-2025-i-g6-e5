a895dd49abc4bd64a36c3ad10549e3e330c611ce
ElKakoGazapo <alequinteroro@unal.edu.co>
2025-07-02 22:30:40 -0500

    topsumrest (7)

--- src/sum4b_estruc.v
+++ src/sum4b_estruc.v
@@ -13,27 +13,26 @@ module sum5b_estruc(
     wire c0; wire c1; wire c2;
 
     sum1b_estruc S0 (.A(A[0]),
-                    .B(B[0]),
-                    .Ci(Ci),
-                    .Cout(c0),
-                    .S(S[0]));
+                     .B(B[0]),
+                     .Ci(Ci),
+                     .Cout(c0),
+                     .S(S[0]));
 
     sum1b_estruc S1 (.A(A[1]),
-                    .B(B[1]),
-                    .Ci(c0),
-                    .Cout(c1),
-                    .S(S[1]));
+                     .B(B[1]),
+                     .Ci(c0),
+                     .Cout(c1),
+                     .S(S[1]));
     
-    sum1b_estruc S0 (.A(A[2]),
-                    .B(B[2]),
-                    .Ci(c1),
-                    .Cout(c2),
-                    .S(S[2]));
-
-    sum1b_estruc S0 (.A(A[3]),
-                    .B(B[3]),
-                    .Ci(c2),
-                    .Cout(Cout),
-                    .S(S[3]));
-
+    sum1b_estruc S2 (.A(A[2]),
+                     .B(B[2]),
+                     .Ci(c1),
+                     .Cout(c2),
+                     .S(S[2]));
+    
+    sum1b_estruc S3 (.A(A[3]),
+                     .B(B[3]),
+                     .Ci(c2),
+                     .Cout(Cout),
+                     .S(S[3]));
 endmodule
